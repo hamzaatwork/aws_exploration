93c83ec74b5ce9b835a1c6faf935a4506fc18487
hamza <hamzaatwork@proton.me>
2022-06-22 15:07:27 +0500

    u

---
+++ source/modules/explore_api.py
@@ -0,0 +1,85 @@
+import boto3
+import json
+
+def explore_api(api_id):
+    print('Server : ', 'client/connection to apigateway')
+    api = boto3.client('apigateway')
+
+    print('Server : ', 'getting all resources in api gateway')
+    api_resources_response = api.get_resources(
+        restApiId=api_id
+    )
+
+    if (api_resources_response['ResponseMetadata']['HTTPStatusCode'] == 200):
+        print('Server : ', 'Server respond OK, continuing..')
+    else:
+        print('Server : ', 'Server respond ', api_resources_response['ResponseMetadata']['HTTPStatusCode'], 'closing..')
+
+    print('Server : ', 'extracting just the resources details from get_resources response, stored in items key')
+    api_resources_details = []
+    for item in api_resources_response['items']:
+        api_resources_details.append(item)
+
+    print('Server : ', 'extracting only those api resources for which some methods exist')
+    api_resources_with_methods = []
+    for resource in api_resources_details:
+        if 'resourceMethods' in resource.keys():
+            api_resources_with_methods.append(resource)
+
+    print('Server : ', 'getting details of methods in api resources using get_method')
+    resource_methods_details = []
+    for resource in api_resources_with_methods:
+        for method in resource['resourceMethods'].keys():
+            get_method_response = api.get_method(
+                restApiId=api_id,
+                resourceId=resource['id'],
+                httpMethod=method
+            )
+            resource_methods_details.append(get_method_response)
+
+    print('Server : ', 'extracting the key methodIntegration from method details')
+    resource_methods_methodIntegration_details = []
+    for method in resource_methods_details:
+        resource_methods_methodIntegration_details.append(method['methodIntegration'])
+
+    print('Server : ', 'extracting arns from all methods integration types except MOCK')
+    resource_methods_except_mock = []
+    for method in resource_methods_methodIntegration_details:
+        if method['type'] != 'MOCK':
+            resource_methods_except_mock.append(method)
+
+    print('Server : ', 'extracting uris and credentials pairs from all methods except MOCK')
+    api_methods_uris_credentials = []
+    for method in resource_methods_except_mock:
+
+        method_type = ''
+        if ':lambda:' in method['uri']:
+            method_type = 'lambda'
+        elif ':states:' in method['uri']:
+            method_type = 'state'
+        else:
+            method_type = 'unknown'
+
+        print('Server : ', 'reducing method uri to contain only relavent arn')
+        uri = method['uri']
+        if uri.count('arn:aws:') > 1:
+            uri_last_portion = uri.rsplit('/', 1)[-1]  # extracting last piece of the url(arn)
+            uri = uri[:len(uri)-len(uri_last_portion)-1] #extra -1 to remove the slash at the end as well
+            while 'arn:aws:' not in uri_last_portion:
+                uri_last_portion = uri.rsplit('/', 1)[-1]  # extracting last piece of the url(arn)
+            method['uri'] = uri_last_portion
+
+        if 'credentials' in method.keys():
+            api_methods_uris_credentials.append({
+                'method_type': method_type,
+                'uri': method['uri'],
+                'credentials': method['credentials']
+            })
+        else:
+            api_methods_uris_credentials.append({
+                'method_type': method_type,
+                'uri': method['uri'],
+                'credentials': 'no_credentials'
+            })
+
+    return api_methods_uris_credentials
---
+++ source/visualize.py
@@ -0,0 +1,162 @@
+import json
+
+data = []
+with open('discovery_reports.json', 'r') as f:
+    data = json.load(f)
+
+visualizable_data = {}
+nodes = []
+links = []
+counter = 0
+
+already_included_arns_list = []
+already_included_arns_names_list = {}
+
+external_node_name = 'external-1'
+external_node_attached = False
+nodes.append({
+            'name' : 'external-1',
+            'meta' : {
+                'description' : 'connection outside of the network'
+            },
+            'icon': './images/ix.png'
+        })
+
+for data in data[0]['Mapping']:
+    counter += 1
+
+    data_keys = list(data.keys())
+
+    source_name = str(data[data_keys[0]])
+    source_icon = './images/'+str(data[data_keys[0]])+'.png'
+    source_arn = str(data[data_keys[1]])
+    target_name = str(data[data_keys[2]])
+    target_icon = './images/'+str(data[data_keys[2]])+'.png'
+    target_arn = str(data[data_keys[3]])
+
+    #excluding results which point to generic resource
+    if source_arn == '*' or target_arn == '*' or ':*:' in source_arn or ':*:' in target_arn:
+        continue
+    #excluding policies from mapping
+    if ':policy/' in source_arn or ':policy/' in target_arn:
+        continue
+
+    #mapping resource names to images
+    if 'arn:aws:s3:' in source_arn:
+        source_icon = './images/s3.png'
+        source_name = 's3'
+    if 'arn:aws:s3:' in target_arn:
+        target_icon = './images/s3.png'
+        target_name = 's3'
+
+    if ':apigateway:*' in source_arn:
+        source_icon = './images/api.png'
+        source_name = 'api'
+    if ':apigateway:*' in target_arn:
+        target_icon = './images/api.png'
+        target_name = 'api'
+
+    if 'arn:aws:lambda:' in source_arn:
+        source_icon = './images/lamda.png'
+        source_name = 'lamda'
+    if 'arn:aws:lambda:' in target_arn:
+        target_icon = './images/lamda.png'
+        target_name = 'lamda'
+
+    if 'arn:aws::ec2:' in source_arn:
+        source_icon = './images/ec2.png'
+        source_name = 'ec2'
+    if 'arn:aws::ec2:' in target_arn:
+        target_icon = './images/ec2.png'
+        target_name = 'ec2'
+
+    if 'arn:aws:states:' in source_arn:
+        source_icon = './images/state.png'
+        source_name = 'state'
+    if 'arn:aws:states:' in target_arn:
+        target_icon = './images/state.png'
+        target_name = 'state'
+
+    if 'arn:aws:iam:' in source_arn:
+        source_icon = './images/iam.png'
+        source_name = 'iam'
+    if 'arn:aws:iam:' in target_arn:
+        target_icon = './images/iam.png'
+        target_name = 'iam'
+
+    if ':policy/' in source_arn:
+        source_icon = './images/policy.png'
+        source_name = 'policy'
+    if ':policy/' in target_arn:
+        target_icon = './images/policy.png'
+        target_name = 'policy'
+
+    if 'arn:aws:ssm:' in source_arn:
+        source_icon = './images/ssm.png'
+        source_name = 'ssm'
+    if 'arn:aws:ssm:' in target_arn:
+        target_icon = './images/ssm.png'
+        target_name = 'ssm'
+
+
+    source_name = str(source_name + '-' + str(counter))
+    target_name = str(target_name + '-' + str(counter))
+
+    # merging duplicate resources
+    sourceDuplicate = False
+    targetDuplicate = False
+    for arn in already_included_arns_list:
+        if source_arn == arn:
+            source_name = already_included_arns_names_list[str(source_arn)]
+            sourceDuplicate = True
+        if target_arn == arn:
+            target_name = already_included_arns_names_list[str(target_arn)]
+            targetDuplicate = True
+
+
+    if sourceDuplicate == False:
+        nodes.append({
+            'name' : source_name,
+            'meta' : {
+                'description' : source_arn
+            },
+            'icon': source_icon
+        })
+    if targetDuplicate == False:
+        nodes.append({
+            'name' : target_name,
+            'meta' : {
+                'description' : target_arn
+            },
+            'icon': target_icon
+        })
+
+
+    #connecting external node with first node(api)
+    if external_node_attached == False:
+        external_node_attached = True
+        links.append({
+            'source' : external_node_name,
+            'target' : source_name
+        })
+
+    links.append({
+        'source' : source_name,
+        'target' : target_name
+    })
+
+
+    already_included_arns_list.append(source_arn)
+    already_included_arns_list.append(target_arn)
+    already_included_arns_names_list[source_arn] = source_name
+    already_included_arns_names_list[target_arn] = target_name
+
+visualizable_data = {
+    'nodes' : nodes,
+    'links' : links
+}
+
+with open('../visualizable_data.json', 'w') as f:
+    f.write(json.dumps(visualizable_data, indent=4, default=str))
+
+# python -m http.server
